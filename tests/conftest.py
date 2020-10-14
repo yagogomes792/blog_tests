23b2cd9ff1a8f84a375fc54bfe16ba1a8d7b22a5
yagogomes792 <yago.792@hotmail.com>
2020-10-14 15:47:56 -0300

    alterado prota

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,5 @@ from time import sleep
 @pytest.fixture(scope="class")
 def setupBrowser(request):
     driver = webdriver.Chrome(executable_path='C://Users//Yago//Desktop//chromedriver.exe')
-    driver.get('http://localhost:5001')
+    driver.get('http://localhost:5000')
     request.cls.driver = driver
